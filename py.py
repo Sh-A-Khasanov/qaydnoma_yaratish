d36fe19d296a3bcce430aaab44cbee2897e2aefc
Sh-A-Khasanov <sherxaan4@gmail.com>
2025-04-26 09:29:48 +0500

    to'liq kod

--- py.py
+++ py.py
@@ -596,6 +596,29 @@ def print_word():
     output_path = os.path.join("data", filename)
     doc.save(output_path)
     print(f"✅ Word hujjat yaratildi: {output_path}")
+    send_file_to_telegram_group(output_path)
+
+import requests
+
+def send_file_to_telegram_group(file_path):
+    bot_token = "7988992200:AAHguvG_iPE6ZG6gdsCjWKJ17fv6vpPzJsQ"
+    chat_id = "-1002635664827"  # Guruh ID
+    url = f"https://api.telegram.org/bot{bot_token}/sendDocument"
+
+    with open(file_path, "rb") as f:
+        files = {"document": f}
+        data = {"chat_id": chat_id}
+        response = requests.post(url, data=data, files=files)
+
+    if response.status_code == 200:
+        print("✅ Hujjat data papkaga yuborildi!")
+    else:
+        print("❌ Hujjat Hujjat data papkaga yuborishda xatolik:", response.text)
+
+# Word hujjat yaratib bo‘lgandan so‘ng chaqirasiz:
+
+
+
 
 
 
